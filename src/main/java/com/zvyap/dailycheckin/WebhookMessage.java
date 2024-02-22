59aa7e5e8d5f7138e445b81db8719feea42818ed
Niskala Airaha <34799053+niskala5570@users.noreply.github.com>
2024-02-22 22:14:17 +0800

    Change webhook to Malay Jawi

--- src/main/java/com/zvyap/dailycheckin/WebhookMessage.java
+++ src/main/java/com/zvyap/dailycheckin/WebhookMessage.java
@@ -1,17 +1,17 @@
 package com.zvyap.dailycheckin;
 
 public enum WebhookMessage {
-    WEBHOOK_TITLE("Daily check in task [{1}]"), // 0 - uid, 1 - in game name, 2 - region, 3 - level, for highest level account
-    WEBHOOK_SUCCESS("Daily checked in successfully"),
-    WEBHOOK_ALREADY_CHECK_IN("Already checked in"),
-    WEBHOOK_FAILED("Daily check in failed - {0},{1}"), //message and retcode
-    WEBHOOK_CAPTCHA("You Hit Captcha! Please check in manually"),
+    WEBHOOK_TITLE("توݢسن دفتر هارين اونتوق [{1}]"), // 0 - uid, 1 - in game name, 2 - region, 3 - level, for highest level account
+    WEBHOOK_SUCCESS("برجاي مندفتر ماسوق!"),
+    WEBHOOK_ALREADY_CHECK_IN("اوق ده دفتر ماسوق هاريني..."),
+    WEBHOOK_FAILED("دفتر ماسوق ݢاݢل - {0},{1}"), //message and retcode
+    WEBHOOK_CAPTCHA("اد Captcha! تولوڠ سلسايکنڽ سنديري"),
     FIELD_GAME("Game"),
-    FIELD_REWARD_TODAY("Reward Today"),
-    FIELD_REWARD_TOMORROW("Reward Tomorrow"),
-    FIELD_DAYS_CHECKED_IN("Days Checked In"),
-    FIELD_DAYS_MISSING("Days Missing"),
-    FIELD_OFFICIAL_MESSAGE("Official Message");
+    FIELD_REWARD_TODAY("ݢنجرن هاري اين"),
+    FIELD_REWARD_TOMORROW("ݢنجرن ايسوق"),
+    FIELD_DAYS_CHECKED_IN("هاري يڠ تله ددفتر ماسوق"),
+    FIELD_DAYS_MISSING("هاري ترتيڠݢل"),
+    FIELD_OFFICIAL_MESSAGE("ڤسنن راسمي");
 
     private final String message;
 
